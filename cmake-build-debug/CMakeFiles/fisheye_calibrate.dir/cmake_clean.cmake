33102048c32d6f3f1ddf2cb39b57bdf14e0296a2
lgx <lgx@qq.com>
2020-08-31 10:26:34 +0800

    1st commit

---
+++ cmake-build-debug/CMakeFiles/fisheye_calibrate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fisheye_calibrate.dir/main.cpp.o"
+  "fisheye_calibrate"
+  "fisheye_calibrate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fisheye_calibrate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
